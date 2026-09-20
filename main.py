31a4e2e71f7b2870424ae6ea7dded21e50e5c4d4
wlodek2106-cyber <wlodek2106@gmail.com>
2026-09-20 10:57:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def cmd_start(message: types.Message):
             [InlineKeyboardButton(text="🎮 Play", url="https://t.me/Zer0lifelabs_ai_bot")],
             [InlineKeyboardButton(text="💎 Buy ZRL", url="https://www.ponsfamily.com/launchpad/0x09bbf85C1C1ad7518847733fc64e161557056200")],
             [InlineKeyboardButton(text="☕ Donate (SOL, ETH, BNB)", callback_data="donate_info")],
-            # 👇 ВОТ НОВАЯ КНОПКА РОДМАПА 👇
+            # Кнопка роадмапа в стартовом меню
             [InlineKeyboardButton(text="Roadmap ZRL 2026-2027 🗺️", callback_data="send_roadmap_pdf")]
         ]
     )
@@ -54,11 +54,11 @@ async def process_donate(callback: types.CallbackQuery):
     await callback.message.answer(donate_text, parse_mode="Markdown")
     await callback.answer()
 
-# 👇 ВОТ НОВЫЙ ОБРАБОТЧИК ДЛЯ ОТПРАВКИ PDF 👇
+# Обработчик для отправки PDF с обновленным file_id
 @dp.callback_query(F.data == "send_roadmap_pdf")
 async def process_roadmap(callback: types.CallbackQuery):
     await callback.answer() # Убираем часики загрузки с кнопки
-    roadmap_file_id = "BQACAgIAAxkBAAIrUGqvljpeOrUy6gefjzpFea_tOn7dAALVogAC7ol5SSZ80YFFdnaQPQQ"
+    roadmap_file_id = "BQACAgIAAxkBAAIraGqvnxP_eZvrrmMF9FkLNGKbK9F7AAIrowAC7ol5SZM2l2KFXMD3PQQ"
     await callback.message.answer_document(
         document=roadmap_file_id,
         caption="Roadmap ZRL 2026-2027 🗺️"
